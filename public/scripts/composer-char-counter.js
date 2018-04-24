a2a61e3a832e76e816c0970bd2a48984e3bb136a
Aden Collinge <aden.collinge@gmail.com>
2018-04-24 15:10:02 +0000

    char counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
+// displays number of characters. red when invalid
   $(".new-tweet textarea").on('keyup', function(){
-    $(".counter").text($(this).val().length);
+    let chars = $(this).val().length;
+    const counter = $(this).siblings(".counter");
+    counter.text(chars);
+// ?? should i add a class here to update color ... ??
+    chars > 140 ? counter.css('color', 'red') : counter.css('color', 'black');
   });
+
 });
 
 
