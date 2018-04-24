e528d2292ee874b13ee4c87a0b56ebd95bfda44e
Aden Collinge <aden.collinge@gmail.com>
2018-04-24 14:36:54 +0000

    new improved char counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,7 @@
 $(document).ready(function(){
-  var count = 0;
-  $(".new-tweet textarea").on("keydown", function(event){
-    if(event.keyCode == 8){
-      if(count > 0){
-        count--;
-      }
-    } else {
-      count++;
-    }
-    $(".new-tweet .counter").text(count);
+  $(".new-tweet textarea").on('keyup', function(){
+    $(".counter").text($(this).val().length);
   });
-
 });
 
 
